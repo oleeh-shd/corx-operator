6d770a3790dfdfebf5ed4ada980affacc1ab7dab
valentynYelkin <valentinyelkin2022@gmail.com>
2023-03-06 13:41:29 +0200

    update operation result component

--- src/pages/Home/components/OperationResultScreen/OperationResultScreen.tsx
+++ src/pages/Home/components/OperationResultScreen/OperationResultScreen.tsx
@@ -1,40 +1,54 @@
-import React, { FC } from 'react';
-import { Pixelify } from 'react-pixelify';
+import cn from 'classnames';
+import React, { FC, useEffect, useState } from 'react';
 
 import 'react-circular-progressbar/dist/styles.css';
+import { Pixelify } from 'react-pixelify';
 import loaderImage from '../../../../assets/loader.png';
-import cn from 'classnames';
-import styles from './resultScreen.module.scss';
-import warning from '../../../../assets/warning.svg';
 import restart from '../../../../assets/restart.svg';
 import unanimous from '../../../../assets/unanimous.jpg';
+import warning from '../../../../assets/warning.svg';
+import { useAgeInfoStore } from '../../../../stores/useAgeInfo';
+import { useGenderInfoStore } from '../../../../stores/useGenderInfo';
+import { useOperationInfoStore } from '../../../../stores/useOperationInfo';
+import { useVoiceToFaceInfoStore } from '../../../../stores/useVoiceToFaceInfo';
+import { TaskStatuses } from '../../../../utils/enum/taskStatuses';
 
 import { period } from '../../../../utils/helpers/period';
+import styles from './operationResultScreen.module.scss';
 
 type ProgressBarProps = {
-  result: string;
-  isActiveGenFace: boolean
-  genFace?: {
-    img: string;
-    default: boolean;
-  },
-  ageGander?: {
-    age: string;
-    gender: string;
-  },
-  pixelSize: number;
-  isActiveAgeGender: boolean;
-
+   result: string;
+   onReset: () => void;
+   pixelSize?: number;
 }
-const ResultScreen: FC<ProgressBarProps> = ({
-      isActiveGenFace,
-      result,
-      genFace,
-      isActiveAgeGender,
-      pixelSize,
-      ageGander = {img: unanimous, default: false}
+const OperationResultScreen: FC<ProgressBarProps> = ({
+       result,
+       onReset,
+       pixelSize = 10,
 
-                                            }) => {
+}) => {
+  const age = useAgeInfoStore((state) => state.task_data.age);
+  const gender = useGenderInfoStore((state) => state.task_data.gender);
+  const image_url = useVoiceToFaceInfoStore((state) => state.task_data.image_url);
+
+  const isActiveGenFace = useOperationInfoStore((state) => state.isActiveGenFace);
+  const isActiveAgeGender = useOperationInfoStore((state) => state.isActiveAgeGender);
+
+  const imgTaskStatus = useVoiceToFaceInfoStore((state) => state.task_status);
+
+
+  const [pixelationSize, setPixelSize] = useState(pixelSize);
+
+  useEffect(() => {
+    if (image_url) {
+      if (!pixelSize) return;
+
+      const intervalId = setInterval(() => {
+        setPixelSize(pixelationSize - 0.20);
+      }, 50);
+      return () => clearInterval(intervalId);
+    }
+  }, [pixelationSize, image_url]);
 
   return (
     <div className={styles.resultFaceWrapper}>
@@ -57,22 +71,24 @@ const ResultScreen: FC<ProgressBarProps> = ({
         } : {}}>
           {result !== '/' && isActiveGenFace &&
 						<div className={styles.imageContainer}>
-              {genFace ? (genFace.default ?
+              {imgTaskStatus === TaskStatuses.FINISHED ? (!image_url ?
                   <img
                     style={{border: '1px solid #C0C0C0'}}
                     src={unanimous}
                     width={93}
                     height={93}
+                    alt="unanimous"
                   /> :
                   <Pixelify
-                    src={genFace.img}
+                    src={image_url}
                     width={93}
                     height={93}
                     centered={true}
-                    pixelSize={pixelSize}
+                    pixelSize={pixelationSize}
                     fillTransparencyColor="white"
                   />) :
                 <img
+                  style={{marginRight: '9px'}}
                   className={styles.additionalLoader}
                   src={loaderImage}
                   alt=""
@@ -83,12 +99,12 @@ const ResultScreen: FC<ProgressBarProps> = ({
               <div className={styles.contentTextWrapper}>
                 <div className={styles.titleWrapper}>Age</div>
                 <div
-                  className={styles.titleContent}>{ageGander?.age ? (period(+ageGander?.age?.substr(2, 2))) : '?'}</div>
+                  className={styles.titleContent}>{age ? (period(+age)) : '?'}</div>
               </div>
               <div className={styles.contentTextWrapper}>
                 <div className={styles.titleWrapper}>Gender</div>
                 <div
-                  className={styles.titleContent}>{ageGander?.gender ? (ageGander.gender === '[0]' ? 'male' : 'female') : '?'}</div>
+                  className={styles.titleContent}>{gender ? (gender === '[0]' ? 'male' : 'female') : '?'}</div>
               </div>
             </div>
           </div>)}
@@ -100,9 +116,7 @@ const ResultScreen: FC<ProgressBarProps> = ({
 				<div className={styles.actionWrapper}>
 					<div
 						className={styles.actionContainer}
-						onClick={(value) => {
-              console.log('Reset:', value)
-            }}>
+						onClick={onReset}>
 						<img src={restart} alt="restart"/>
 						<div className={styles.restartTitle}>
 							Restart verification
@@ -115,4 +129,4 @@ const ResultScreen: FC<ProgressBarProps> = ({
   );
 };
 
-export default ResultScreen;
+export default OperationResultScreen;
